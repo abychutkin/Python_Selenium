cc2976d229894e345de4a4d5f2f8a2eb2fa077bb
abychutkin <abychutkin@gmail.com>
2019-06-25 20:38:37 +0300

    Tetisting alert windows

---
+++ alert.py
@@ -0,0 +1,24 @@
+import math
+from selenium import webdriver 
+
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+
+browser = webdriver.Chrome()
+browser.get('http://suninjuly.github.io/alert_accept.html')
+
+submit = browser.find_element_by_css_selector('button.btn')
+submit.click()
+
+alert = browser.switch_to.alert
+alert.accept()
+
+data = browser.find_element_by_id('input_value').text
+
+input_field = browser.find_element_by_id('answer')
+input_field.send_keys(calc(data))
+
+submit = browser.find_element_by_css_selector('button.btn')
+submit.click()
